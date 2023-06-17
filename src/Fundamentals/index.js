f9770f80b4d7d40611ce129e56e92ee0d3a6c2fd
Abdjalil Brihoum <abdjalil@brihoum.com>
2023-06-17 19:59:41 +0100

    add dev tools

--- src/Fundamentals/index.js
+++ src/Fundamentals/index.js
@@ -1,12 +1,12 @@
-import { addBug, removeBug, resolveBug } from "./actions";
 import store from "./store";
+import { addBug, removeBug, resolveBug } from "./actions";
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(addBug("BUG 1"));
+store.dispatch(addBug("BUG 2"));
+store.dispatch(addBug("BUG 3"));
 
 store.dispatch(resolveBug(1));
-
-store.dispatch(removeBug(1));
