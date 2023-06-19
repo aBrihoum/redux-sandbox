b9929f5c171dc40e3d71d84a558b4d18504b222d
Abdjalil Brihoum <abdjalil@brihoum.com>
2023-06-19 18:05:06 +0100

    combined stores

---
+++ src/4- toolkit/store/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import bugsReducer from "./bugs";
+import projectsReducer from "./projects";
+
+const reducer = combineReducers({
+  bugs: bugsReducer,
+  projects: projectsReducer,
+});
+
+export default reducer;
